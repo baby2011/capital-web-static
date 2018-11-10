c7ab48b17d3f0929c849c40dbda60eb972d8ee37
baby2011 <877078718@qq.com>
2018-11-11 00:08:32 +0800

    lang

---
+++ src/i18n/messages.js
@@ -0,0 +1,20 @@
+export default {
+  en: {
+    slogan: 'Chain Capital',
+    sideBar: {
+      banner: 'Banner',
+      team: 'Team',
+      portfolio: 'Portfolio',
+      community: 'Community'
+    }
+  },
+  cn: {
+    slogan: '区块资本',
+    sideBar: {
+      banner: '头图管理',
+      team: '团队管理',
+      portfolio: '投资管理',
+      community: '社区管理'
+    }
+  }
+};
